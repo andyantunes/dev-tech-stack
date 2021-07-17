a56008f1774a5e091707bd17470c338169fcc890
unknown <andersonlantunes@gmail.com>
2021-07-17 15:19:10 -0300

    Created a new Expo app

---
+++ config/config.js
@@ -0,0 +1,12 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyB2Us6KB4Z3EnJ5RpQZn9bylty1bhJ4S9c',
+  authDomain: 'dev-mobile-i.firebaseapp.com',
+  databaseURL: 'https://dev-mobile-i-default-rtdb.firebaseio.com',
+  projectId: 'dev-mobile-i',
+  storageBucket: 'dev-mobile-i.appspot.com',
+  messagingSenderId: '1024723145142',
+  appId: '1:1024723145142:web:6479a7dd3c57448d57b117',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
